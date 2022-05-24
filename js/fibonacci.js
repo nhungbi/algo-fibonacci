7e9e1eb291824e2e608487da165e648e12fab4de
nhungbi <nhungvan16@gmail.com>
2022-05-24 11:10:21 -0500

    fn both

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,10 @@
 const fibonacci = (num) => {
+    if (num <= 1) {
+        return num
+    }
+    return fibonacci(num-1) + fibonacci(num-2)
     
-}
+};
 
 module.exports = {fibonacci}
+
--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,7 @@
 def fibonacci(n):
-  pass
+  if n <= 1:
+    return n
+  
+  return fibonacci(n-1) + fibonacci(n-2)
 
 
